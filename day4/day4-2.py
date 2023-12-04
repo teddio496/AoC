4a1b2dda8b46a4f031d8b0011d19c82bbc0091dd
teddio496 <teddio496@gmail.com>
2023-12-04 12:52:34 -0500

    day 3/4 inprogress

--- day4/day4-2.py
+++ day4/day4-2.py
@@ -2,17 +2,22 @@ with open("day4input2.txt", "r") as file:
     result = [x for x in file.read().strip().splitlines()]
 
 answer = 0
-map = {}
+map = {1:1}
 for i in result:
     temp = 0
     nums = i.split(":")[1].split("|")
+    game = int(i.split(":")[0][5:]) 
     pool = nums[0].split()
     picks = nums[1].split()
+
     for pick in picks:
         if pick in pool:
             temp += 1
-    for j in temp:
-        
+    print(temp)
+    for j in range(game+1, game + temp+1):
+        map[j] = map.get(j, 1) + (map.get(game, 1))
 
-
-print(answer)
+acc = 1
+for i in map.values():
+    acc += i
+print(acc)
